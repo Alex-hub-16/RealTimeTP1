473008a0795e918f829c64246ce1976496e26464
Alex <abrochardpro@gmail.com>
2023-12-18 09:24:58 +0100

    new_files

---
+++ compare.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   
+   int a;
+   int b;
+   
+   a = 5;
+   b = 5;
+   
+   if (a > b) {
+   
+   	printf("%d ", a);
+   
+   }
+   else if (a == b)
+   {
+   	printf("Values are equals");
+   } 
+   else 
+   {
+   	printf("%d ", b);
+   }
+   
+   
+   return 0;
+}
---
+++ sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   
+   int a;
+   int b;
+   int somme;
+   
+   a = 5;
+   b = 2;
+   
+   somme = a+b;
+   
+   printf("%d ", a);
+   printf("%d ", b);
+   printf("%d ", somme);
+   
+   return 0;
+}
